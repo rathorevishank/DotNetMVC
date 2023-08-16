307e53b9bc9efe1db3d4412277d2bb082b2061a2
Vishank Rathore <124240514+rathorevishank@users.noreply.github.com>
2023-08-17 01:14:31 +0530

    Add project files.

---
+++ DemoDotNetMVC/Data/MVCDemoDbContext.cs
@@ -0,0 +1,13 @@
+﻿using DemoDotNetMVC.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoDotNetMVC.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employess {  get; set; }
+    }
+}
---
+++ DemoDotNetMVC/Models/Domain/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace DemoDotNetMVC.Models.Domain
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public long Salary { get; set; }
+        public DateTime DateOfBirth { get; set;}
+        public String Department { get; set; }
+
+    }
+}
